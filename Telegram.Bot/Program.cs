5a42ed1a439e38819344e48bc43ce10eafa58d8e
Zahar Sysyuk <vip.sysyuk@mail.ru>
2021-10-23 21:36:50 +0700

    Add project files.

---
+++ Telegram.Bot/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace MyTelegram.Bot
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			
+		}
+	}
+}
