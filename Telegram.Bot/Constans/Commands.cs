72e47617ce264030f7c9057d482546374e9f94ea
Zahar Sysyuk <vip.sysyuk@mail.ru>
2021-10-23 22:20:30 +0700

    added commands for telegram

---
+++ Telegram.Bot/Constans/Commands.cs
@@ -0,0 +1,12 @@
+﻿namespace MyTelegram.Bot.Constans
+{
+	public class Commands
+	{		
+		public static string[] Help = { "/помощь", "/ку", "/привет" };
+		public const string Info = "/инфо";
+		public const string Schedule = "/расписание";
+		public const string DaysDating = "/встречаемся";
+		public const string Motivation = "/мотивация";
+		public const string AddMotivation = "/добавить-мотивацию";
+	}
+}
